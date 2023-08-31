3fd9a9b1e905a8857dd946c5b082f0c421bab656
Brian <wamwangimathenge@gmail.com>
2023-08-31 14:52:43 +0300

    Create a dictionary and access some of its values

---
+++ dict.py
@@ -0,0 +1,10 @@
+tel = {
+    "P. McCartney": 123456,
+    "J. Lennon": 987654,
+    "G. Harrison": 113425,
+    "R. Starr": 777888
+}
+
+print(tel)
+print(tel["G. Harrison"])
+print(tel["P. McCartney"])
